dca70ffe0a275d59e65e107dc07538c2d7f8c502
DivyaRavi92 <100188062+DivyaRavi92@users.noreply.github.com>
2022-07-06 15:19:05 +0530

    Update testBMSdata.cpp

--- testBMSdata.cpp
+++ testBMSdata.cpp
@@ -9,7 +9,8 @@ TEST_CASE("TestCase1 : Stream Valid Data From Sensor") {
     char batteryType = "Lead";
     float tempArray[] = {-12.0, 20.0, 40.0, 30.0};
     int socArray[] = {60, 50, 45, 80};
-    REQUIRE(processBMSStreamData(batteryType, tempArray, socArray)==4);
+    int Range = 4;
+    REQUIRE(processBMSStreamData(batteryType, tempArray, socArray, Range)==4);
 }
 
 TEST_CASE("TestCase2 : Stream InValid Data From Sensor and check if the invalid values are ignored") {
@@ -17,5 +18,6 @@ TEST_CASE("TestCase2 : Stream InValid Data From Sensor and check if the invalid 
     char batteryType = "Nickel";
     float tempSensorDataInvalid[] = {50.2, 20.5, -40.0, 9.62};
     int socSensorDataInvalid[] = {20, 65, 45, 10};
-    REQUIRE(processBMSStreamData(batteryType, tempSensorDataInvalid, socSensorDataInvalid)==1);
+    int Range = 4;
+    REQUIRE(processBMSStreamData(batteryType, tempSensorDataInvalid, socSensorDataInvalid, Range)==1);
 }
