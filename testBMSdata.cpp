6920b426ed1871c0afc3600eda93574a4815698f
DivyaRavi92 <100188062+DivyaRavi92@users.noreply.github.com>
2022-07-06 16:11:10 +0530

    Update testBMSdata.cpp

--- testBMSdata.cpp
+++ testBMSdata.cpp
@@ -10,7 +10,7 @@ TEST_CASE("TestCase1 : Stream Valid Data From Sensor") {
     float tempArray[] = {-12.0, 20.0, 40.0, 30.0};
     int socArray[] = {60, 50, 45, 80};
     int Range = 4;
-    REQUIRE(processBMSStreamData(BMSType, tempArray, socArray, Range)==4);
+    REQUIRE(processBMSStreamData(BMSType, tempArray, socArray, Range) == 4);
 }
 
 TEST_CASE("TestCase2 : Stream InValid Data From Sensor and check if the invalid values are ignored") {
@@ -19,5 +19,5 @@ TEST_CASE("TestCase2 : Stream InValid Data From Sensor and check if the invalid 
     float tempSensorDataInvalid[] = {50.2, 20.5, -40.0, 9.62};
     int socSensorDataInvalid[] = {20, 65, 45, 10};
     int Range = 4;
-    REQUIRE(processBMSStreamData(BMSType, tempSensorDataInvalid, socSensorDataInvalid, Range)==1);
+    REQUIRE(processBMSStreamData(BMSType, tempSensorDataInvalid, socSensorDataInvalid, Range) == 1);
 }
