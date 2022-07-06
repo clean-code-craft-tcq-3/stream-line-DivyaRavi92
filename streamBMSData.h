eefda7576b5a9b79a3c2ea6083c302ed4aa52b06
DivyaRavi92 <100188062+DivyaRavi92@users.noreply.github.com>
2022-07-06 15:42:33 +0530

    Update streamBMSData.h

--- streamBMSData.h
+++ streamBMSData.h
@@ -1,9 +1,14 @@
 #include <stdio.h>
+typedef enum
+{
+    LEAD = 0u;
+    NICKEL
+}batteryType_t;
 
 void printOnConsole(float temp, float soc);
-bool checkTempinRange(char batteryType, float temp);
+bool checkTempinRange(batteryType_t batteryType, float temp);
 bool checkThresholdLimit(float minVal, float maxVal, float temp);
 bool checkSOCinRange(int soc);
 bool checkValueInRange(bool isTempValueInRange, bool isSOCValinRange);
-void processBMSStreamData(char batType, float tempArray[], int SOCArray[], int Range);
+void processBMSStreamData(batteryType_t batType, float tempArray[], int SOCArray[], int Range);
 
