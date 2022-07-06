72b7cae597bb7f937e3b7459cc9715072e2a482c
DivyaRavi92 <100188062+DivyaRavi92@users.noreply.github.com>
2022-07-06 15:55:18 +0530

    Update streamBMSData.h

--- streamBMSData.h
+++ streamBMSData.h
@@ -10,5 +10,5 @@ bool checkTempinRange(batteryType_t batteryType, float temp);
 bool checkThresholdLimit(float minVal, float maxVal, float temp);
 bool checkSOCinRange(int soc);
 bool checkValueInRange(bool isTempValueInRange, bool isSOCValinRange);
-void processBMSStreamData(batteryType_t batType, float tempArray[], int SOCArray[], int Range);
+int processBMSStreamData(batteryType_t batType, float tempArray[], int SOCArray[], int Range);
 
