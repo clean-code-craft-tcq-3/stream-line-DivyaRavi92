8b4649c8eda09019ea4013dc2e1dad140e6ef418
DivyaRavi92 <100188062+DivyaRavi92@users.noreply.github.com>
2022-07-06 19:34:47 +0530

    Update testBMSdata.cpp

--- testBMSdata.cpp
+++ testBMSdata.cpp
@@ -2,6 +2,7 @@
 
 #include "test/catch.hpp"
 #include "streamBMSData.h"
+#include "stdbool.h"
 
 
 TEST_CASE("TestCase1 : Stream Number of Valid Data From Sensor is above 50") {
@@ -10,7 +11,7 @@ TEST_CASE("TestCase1 : Stream Number of Valid Data From Sensor is above 50") {
     int temp[2] = {-20, 50};
     int soc[2] = {10, 70};
     int Range = 50;
-    REQUIRE(processBMSStreamData(BMSType, Range, temp, soc) == TRUE);
+    REQUIRE(processBMSStreamData(BMSType, Range, temp, soc) == true);
 }
 
 TEST_CASE("TestCase2 : Stream Number of Valid Data From Sensor is below 50") {
@@ -19,5 +20,5 @@ TEST_CASE("TestCase2 : Stream Number of Valid Data From Sensor is below 50") {
     int temp[2] = {-30, 50};
     int soc[2] = {10, 100};
     int Range = 50;
-    REQUIRE(processBMSStreamData(BMSType, Range, temp, soc) == FALSE);
+    REQUIRE(processBMSStreamData(BMSType, Range, temp, soc) == false);
 }
