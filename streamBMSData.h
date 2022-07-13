79bd8f56b53392da249f7ee5e8e4788cac5f1268
DivyaRavi92 <100188062+DivyaRavi92@users.noreply.github.com>
2022-07-13 10:47:23 +0530

    Update streamBMSData.h

--- streamBMSData.h
+++ streamBMSData.h
@@ -1,17 +1,10 @@
 #include <stdio.h>
 
-
-typedef enum
-{
-    LEAD = 0,
-    NICKEL
-}batteryType_t;
-
 void generateRandomNumbers(int tempMin, int tempMax, int * Array, int Range);
-bool processBMSStreamData(batteryType_t batType, int Range, int * tempRange, int *socRange);
+bool processBMSStreamData(int Range, int * tempRange, int *socRange);
 bool checkThresholdLimit(int minVal, int maxVal, int temp);
-void printOnConsole(int temp, int soc);
-bool checkTempinRange(batteryType_t batteryType, float temp);
+void printOnConsole(int temp[], int soc[]);
+bool checkTempinRange(int temp);
 bool checkSOCinRange(int soc);
 bool checkValueInRange(bool isTempValueInRange, bool isSOCValinRange);
 bool randomValuesRangeCheck(int noOfValidValues);
